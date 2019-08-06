2046379187df50916507efc4742d4246c7b4ac5f
adeliyakyzdarbekova <delya.k.shu@gmail.com>
2019-08-06 15:32:37 +0600

    create particular variables for add method to make more readable

--- app/CatFactory.php
+++ app/CatFactory.php
@@ -6,18 +6,29 @@ use App\Interfaces\IFactory;
 
 class CatFactory implements IFactory
 {
-
+    /**
+     * create cat(s) by amount
+     * @param int $catAmount
+     * @return array|mixed
+     */
     static public function create(int $catAmount)
     {
+        $cats = [];
+
         $breed = ['abyssinian cat','american Bobtail','american curl','american shorthair','american wirehair'];
         $gender =['female', 'male'];
         $color = ['red', 'black', 'white', 'silver','brown'];
         $name = ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'];
 
+        $catBreed = $breed[array_rand($breed,1)];
+        $catAge = rand(1,7);
+        $catGender = $gender[array_rand($gender, 1)];
+        $catColor = $color[array_rand($color,1)];
+        $catName = $name[array_rand($name,1)];
+        $catSquare = rand(100,300);
 
-        $cats = [];
         while ($catAmount > 0) {
-            array_push($cats,new Cat($breed[array_rand($breed,1)],rand(1,7),$gender[array_rand($gender, 1)],$color[array_rand($color,1)],$name[array_rand($name,1)],rand(140,300),rand(100,200)));
+            array_push($cats,new Cat($catBreed,$catAge,$catGender,$catColor,$catName,$catSquare));
             $catAmount--;
         }
         return $cats;
--- app/DogFactory.php
+++ app/DogFactory.php
@@ -1,29 +1,36 @@
 <?php
 
-
 namespace App;
 
-
-use App\Abstraction\ParameterParser;
 use App\Interfaces\IFactory;
 
 class DogFactory implements IFactory
 {
+    /**
+     * create dogs
+     * @param int $dogAmount
+     * @return array|mixed
+     */
     static public function create(int $dogAmount)
     {
+        $dogs = [];
 
         $breed = ['Alaskan Klee Kai','Appenzeller Sennenhunde','Borzoi','Pitbul','Bolognese'];
         $gender =['female', 'male'];
         $color = ['red', 'black', 'white', 'silver','brown'];
         $name = ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'];
 
-        $dogs = [];
+        $dogBreed = $breed[array_rand($breed,1)];
+        $dogAge = rand(1,7);
+        $dogGender = $gender[array_rand($gender, 1)];
+        $dogColor = $color[array_rand($color,1)];
+        $dogName = $name[array_rand($name,1)];
+        $dogSquare = rand(100,300);
+
         while ($dogAmount > 0) {
-            array_push($dogs,new Dog($breed[array_rand($breed,1)],rand(1,7),$gender[array_rand($gender, 1)],$color[array_rand($color,1)],$name[array_rand($name,1)],rand(140,300),rand(100,300)));
+            array_push($dogs,new Dog($dogBreed,$dogAge,$dogGender,$dogColor,$dogName,$dogSquare));
             $dogAmount--;
         }
-
-
         return $dogs;
     }
 }
