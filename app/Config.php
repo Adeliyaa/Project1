cfb9b46ce84eb51e6fa0ab424c24a2b1261f0f7c
adeliyakyzdarbekova <delya.k.shu@gmail.com>
2019-08-07 18:03:29 +0600

    add Config class.

---
+++ app/Config.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+class Config
+{
+    /**
+     * get parameter values
+     * @param string $key
+     * @return mixed|null
+     */
+    public static function get(string $key)
+    {
+        $config = [
+            'cat_breed'       => ['abyssinian cat','american Bobtail','american curl','american shorthair','american wirehair'],
+            'cat_color'       => ['red', 'black', 'white', 'silver','brown'],
+            'cat_name'        => ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'],
+            'cat_age'         => rand(1,7),
+            'cat_square'      => rand(50, 250),
+            'dog_breed'       => ['Alaskan Klee Kai','Appenzeller Sennenhunde','Borzoi','Pitbul','Bolognese'],
+            'dog_color'       => ['red', 'black', 'white', 'silver','brown'],
+            'dog_name'        => ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'],
+            'dog_age'         => rand(1, 10),
+            'dog_square'      => rand (100, 300),
+            'gender'          => ['female', 'male'],
+            'box_limit_crap'  => 200,
+            'room_limit_crap' => 400,
+            'current_space'   => 0,
+            'puppy_count'     => 0,
+            'kitty_count'     => 0,
+            'box_square'      => 1200,
+        ];
+
+        $value = isset($config[$key]) ? $config[$key] : null;
+
+        return $value;
+    }
+}
