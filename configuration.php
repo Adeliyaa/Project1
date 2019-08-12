940bb0f6dcf5e38c5b45b201320f274404039f22
adeliyakyzdarbekova <delya.k.shu@gmail.com>
2019-08-12 14:29:57 +0600

    add configuration.php file.

---
+++ configuration.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var array $config for holfing parameter values*/
+return $config = [
+    'cat_breed'       => ['abyssinian cat','american Bobtail','american curl','american shorthair','american wirehair'],
+    'cat_color'       => ['red', 'black', 'white', 'silver','brown'],
+    'cat_name'        => ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'],
+    'cat_age'         => rand(1,7),
+    'cat_square'      => rand(50, 250),
+    'dog_breed'       => ['Alaskan Klee Kai','Appenzeller Sennenhunde','Borzoi','Pitbul','Bolognese'],
+    'dog_color'       => ['red', 'black', 'white', 'silver','brown'],
+    'dog_name'        => ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'],
+    'dog_age'         => rand(1, 10),
+    'dog_square'      => rand (100, 300),
+    'gender'          => ['female', 'male'],
+    'box_limit_crap'  => 200,
+    'room_limit_crap' => 400,
+    'current_space'   => 0,
+    'puppy_count'     => 0,
+    'kitty_count'     => 0,
+    'box_square'      => 1200,
+];
