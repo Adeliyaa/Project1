d065377e4f1b12840a7b9e8392d6daf5e33de33f
adeliyakyzdarbekova <delya.k.shu@gmail.com>
2019-08-12 14:30:23 +0600

    move config array to  configuration.php file.

--- app/Config.php
+++ app/Config.php
@@ -6,6 +6,22 @@ use Exception;
 
 class Config
 {
+    /** @var array storage of parameter value */
+    private static $config;
+
+    /**
+     * get config array
+     * @return array|mixed
+     */
+    public static function getConfig()
+    {
+        if (self::$config === null) {
+            self::$config = include_once(ROOT.'configuration.php');
+        } else {
+            return self::$config;
+        }
+    }
+
     /**
      * get parameter values
      * @param string $key
@@ -13,29 +29,11 @@ class Config
      */
     public static function get(string $key)
     {
-        $config = [
-            'cat_breed'       => ['abyssinian cat','american Bobtail','american curl','american shorthair','american wirehair'],
-            'cat_color'       => ['red', 'black', 'white', 'silver','brown'],
-            'cat_name'        => ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'],
-            'cat_age'         => rand(1,7),
-            'cat_square'      => rand(50, 250),
-            'dog_breed'       => ['Alaskan Klee Kai','Appenzeller Sennenhunde','Borzoi','Pitbul','Bolognese'],
-            'dog_color'       => ['red', 'black', 'white', 'silver','brown'],
-            'dog_name'        => ['Ilya','Bekbolat','Serzhan','Adeliya','Sasha'],
-            'dog_age'         => rand(1, 10),
-            'dog_square'      => rand (100, 300),
-            'gender'          => ['female', 'male'],
-            'box_limit_crap'  => 200,
-            'room_limit_crap' => 400,
-            'current_space'   => 0,
-            'puppy_count'     => 0,
-            'kitty_count'     => 0,
-            'box_square'      => 1200,
-        ];
+        self::getConfig();
 
         try {
-            if(isset($config[$key])) {
-                $value = $config[$key];
+            if(isset(self::$config[$key])) {
+                $value = self::$config[$key];
 
                 return $value;
             } else {
